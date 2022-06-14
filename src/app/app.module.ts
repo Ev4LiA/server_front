122f27a7e818a3c6c981136f1e271b9d12f22b7e
Ev4LiA <dragonflyer1998@gmail.com>
2022-06-14 21:42:19 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { NotificationModule } from './notification.module';
+import { ServerModalComponent } from './components/server-modal/server-modal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    NotificationModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
